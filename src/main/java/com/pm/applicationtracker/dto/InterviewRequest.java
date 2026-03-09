b1cf8eb4450fb800ff8cc4c6320b60cd07874072
nehyabah <nehyabah@gmail.com>
2026-03-09 09:03:18 +0000

    InterviewDTO

--- src/main/java/com/pm/applicationtracker/dto/InterviewRequest.java
+++ src/main/java/com/pm/applicationtracker/dto/InterviewRequest.java
@@ -1,4 +1,26 @@
 package com.pm.applicationtracker.dto;
 
-public record InterviewRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record InterviewRequest(
+        @NotNull(message ="Date is required")
+        LocalDate date,
+
+        @NotNull(message ="Time is required")
+        LocalTime time,
+        @NotBlank(message ="interviewer name is required")
+        String interviewerName,
+
+        String notes,
+
+        @NotBlank(message = "Interview type is required")
+        String interviewType,
+
+        @NotNull(message = "Application ID is required")
+        Long applicationId
+) {
 }
--- src/main/java/com/pm/applicationtracker/dto/InterviewResponse.java
+++ src/main/java/com/pm/applicationtracker/dto/InterviewResponse.java
@@ -1,4 +1,17 @@
 package com.pm.applicationtracker.dto;
 
-public record InterviewResponse() {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record InterviewResponse(
+        Long id,
+        LocalDate date,
+        LocalTime time,
+        String interviewerName,
+        String notes,
+        String outcome,
+        String interviewType,
+        String applicationPosition
+
+) {
 }
